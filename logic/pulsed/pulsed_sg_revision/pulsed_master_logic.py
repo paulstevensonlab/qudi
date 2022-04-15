19136c6af211673d0b5dc5ce39bc546197dc47a8
Nathaniel Beaver <beaver.n@northeastern.edu>
2022-04-15 18:38:25 -0400

    Paul's work: fix the optimization step.

--- logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
+++ logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
@@ -371,6 +371,9 @@ class PulsedMasterLogic(GenericLogic):
 
             if self._autotrack:
                 if np.mod(self.elapsed_sweeps, self._trackevery) == 0:
+                    if self.scanvar == 'Time':
+                        #self.pulsegenerator().set_continuous_out([1, 1, 1, 0, 0, 0, 0, 0])
+                    return
                     self.module_state.unlock()
                     if self.optimiserlogic().module_state() == 'idle':
                         self.optimiserlogic().start_refocus(caller_tag='tracking')
