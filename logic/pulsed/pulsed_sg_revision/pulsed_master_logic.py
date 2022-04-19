4f4038c7b074673300ddd0bd1d1fd691c998a31f
Nathaniel Beaver <beaver.n@northeastern.edu>
2022-04-19 12:16:15 -0400

    Paul's work, fix this so it re-enables output for optimization when sweeping time for e.g. T1 measurements.

--- logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
+++ logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
@@ -372,8 +372,7 @@ class PulsedMasterLogic(GenericLogic):
             if self._autotrack:
                 if np.mod(self.elapsed_sweeps, self._trackevery) == 0:
                     if self.scanvar == 'Time':
-                        #self.pulsegenerator().set_continuous_out([1, 1, 1, 0, 0, 0, 0, 0])
-                    return
+                        self.pulsegenerator().set_continuous_out([1, 1, 1, 0, 0, 0, 0, 0])
                     self.module_state.unlock()
                     if self.optimiserlogic().module_state() == 'idle':
                         self.optimiserlogic().start_refocus(caller_tag='tracking')
