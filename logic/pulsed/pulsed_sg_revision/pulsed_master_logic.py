6d16300f94a90f763a8430ff704d3c76f11ba1d0
Nathaniel Beaver <beaver.n@northeastern.edu>
2022-05-04 18:21:41 -0400

    Commenting this out for the moment to avoid out-of-memory errors for large numbers of scans or large pulse delays (e.g. 4 ms).

--- logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
+++ logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
@@ -346,8 +346,8 @@ class PulsedMasterLogic(GenericLogic):
 
             self.number_of_lines = int(self.exptparams[4])
             self.pulsed_raw_data = np.zeros((3, self.final_sweep_list.size, self.number_of_lines)) # will save sig, ref, and sig/ref
-            self.tt_output = self.templist = np.zeros(
-                (int(self.number_of_lines), int(self.totaltime+1), int(self.final_sweep_list.size)))
+            # self.tt_output = self.templist = np.zeros(
+            #     (int(self.number_of_lines), int(self.totaltime+1), int(self.final_sweep_list.size)))
             self.sigExptRunningUpdated.emit(True)
             self.sigNextLinePulse.emit()
         return
@@ -422,7 +422,7 @@ class PulsedMasterLogic(GenericLogic):
                     self.pulsed_raw_data[2, k, self.elapsed_sweeps] = (
                                 np.mean(self.fromcounter[0, st_inds[0]:st_inds[1]]) /
                                 np.mean(self.fromcounter[0, end_inds[0]:end_inds[1]]))
-                self.tt_output[self.elapsed_sweeps,:,:] = self.templist
+                # self.tt_output[self.elapsed_sweeps,:,:] = self.templist
 
             elif self.scanvar == 'Freq':
                 self.odmrlogic1()._mw_device.reset_sweeppos()
@@ -780,19 +780,19 @@ class PulsedMasterLogic(GenericLogic):
                                    delimiter='\t',
                                    timestamp=timestamp)
 
-        filepath = self._save_logic.get_path_for_module(module_name='TT_raw')
+        # filepath = self._save_logic.get_path_for_module(module_name='TT_raw')
         if tag is None:
             tag = ''
         expt_add = (self.exptrunning).replace(" ", "")
-        filelabel_raw = '{0}_Pulsed_'.format(tag) + expt_add + '_TT'
-        data_TT = OrderedDict()
-        data_TT['Raw TT'] = self.tt_output
-        self._save_logic.save_data(data_raw,
-                                   filepath=filepath,
-                                   filelabel=filelabel_raw,
-                                   fmt='%.6e',
-                                   delimiter='\t',
-                                   timestamp=timestamp)
+        # filelabel_raw = '{0}_Pulsed_'.format(tag) + expt_add + '_TT'
+        # data_TT = OrderedDict()
+        # data_TT['Raw TT'] = self.tt_output
+        # self._save_logic.save_data(data_raw,
+        #                            filepath=filepath,
+        #                            filelabel=filelabel_raw,
+        #                            fmt='%.6e',
+        #                            delimiter='\t',
+        #                            timestamp=timestamp)
         return
 
 
