d99e5ea81528b4a9fa00f749662163cf6545b0b5
Nathaniel Beaver <beaver.n@northeastern.edu>
2022-05-18 11:58:15 -0400

    Switch to mat-file output since save_data() does not permit 3D arrays.

--- logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
+++ logic/pulsed/pulsed_sg_revision/pulsed_master_logic.py
@@ -30,6 +30,8 @@ import numpy as np
 from core.util.mutex import Mutex
 from core.configoption import ConfigOption
 from core.statusvariable import StatusVar
+import scipy.io
+import os.path
 
 
 class PulsedMasterLogic(GenericLogic):
@@ -804,15 +806,12 @@ class PulsedMasterLogic(GenericLogic):
         if tag is None:
             tag = ''
         expt_add = (self.exptrunning).replace(" ", "")
-        filelabel_raw_TT = '{0}_Pulsed_'.format(tag) + expt_add + '_TT'
-        data_TT = OrderedDict()
-        data_TT['Raw TT'] = self.tt_output
-        self._save_logic.save_data(data_TT,
-                                   filepath=filepath_TT,
-                                   filelabel=filelabel_raw_TT,
-                                   fmt='%.6e',
-                                   delimiter='\t',
-                                   timestamp=timestamp)
+        filename_raw_TT = timestamp.strftime('%Y%m%d-%H%M-%S') + '_' + '{0}_Pulsed_'.format(tag) + expt_add + '_TT' + '.mat'
+        data_TT = {'tt_output': self.tt_output}
+        fullpath_TT = os.path.join(filepath_TT, filename_raw_TT)
+        # _save_logic.save_data does not permit 3D arrays, see e.g.
+        # "Found data array with dimension >2. Unable to save data."
+        scipy.io.savemat(fullpath_TT, data_TT)
         return
 
 
